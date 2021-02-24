0610b94be9b0250b81f92c22cc2431cb838e41ff
mcraet <73713665+mcraeteisha@users.noreply.github.com>
2021-02-23 23:10:51 -0500

    updated js file

--- script.js
+++ script.js
@@ -1,41 +1,59 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
+
+  var lowercaseChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  var uppercaseChar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  var numericChar = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+  var specialChar = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+'];
   var length = Number(prompt("How many characters would you like your password to be? (Please choose between 8-128 characters.)"));
 
   if (length < 8 || length > 128) {
     return "Invalid length. Try again with 8 - 128";
   }
 
+  alert("Great! Your password will have " + length + " characters.");
+
   var useUppercase = confirm ("Would you like to add uppercase letters?");
   var useLowercase = confirm ("Would you like to add lowercase letters?");
   var useSpecialChar = confirm ("Would you like add special characters?");
   var useNumeric = confirm("Would you like to add numbers?")
+   
+  if (useUppercase == false && useLowercase == false && useSpecialChar == false && useNumeric == false) {
+    return "You must pick at least one criteria to generate a unique password. Try again.";
+  }
 
+  var chosenCharacters = [];
 
-  var generatedPassword
+  if (useLowercase) {
+    chosenCharacters = chosenCharacters.concat(lowercaseChar);
+  }
 
-  function declareLowercase() {
-    var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
+  if (useUppercase) {
+    chosenCharacters = chosenCharacters.concat(uppercaseChar);
   }
-  function declareUppercase() {
-    var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  if (useSpecialChar) {
+    chosenCharacters = chosenCharacters.concat(specialChar);
   }
-  function declareNumeric() {
-    var numericChar = "1234567890";
+
+  if (useNumeric) {
+    chosencharacters = chosenCharacters.concat(numericChar);
   }
-  function declareSpecialChar() {
-    var specialChar = "!@#$%^&*";
+
+  console.log(chosenCharacters);
+
+  var generatedPassword = '';
+
+  for (i = 0; i < length; i++) {
+    generatedPassword = generatedPassword + chosenCharacters[Math.floor(Math.random() * chosenCharacters.length)];
+    console.log(generatedPassword);
   }
-  declareLowercase()
-  declareUppercase()
-  declareNumeric()
-  declareSpecialChar()
+  
   return generatedPassword;
 }
 
-// Write password to the #password input
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
